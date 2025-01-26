6de7ead6b103cd00beb977632d5e06a0e81d7297
Atamanyuk2008 <atamaniuk.aleksandr@gmail.com>
2025-01-26 23:10:10 +0200

    Add files via upload

---
+++ prakt16в/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt16в
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int f = 0; f < 6; f++)
+            {
+                for (int s = 0; s <= f; s++)
+                {
+                    Console.Write("5 ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
